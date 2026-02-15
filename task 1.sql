0b17a291a7132e5c68a9823b9086cecd7bf19b07
Stark-ml <zeref.sama.gilgamesh@gmail.com>
2026-02-15 19:01:32 +0300

    init

---
+++ task 1.sql
@@ -0,0 +1,21 @@
+-- CREATE TABLE book (
+-- book_id SERIAL PRIMARY KEY,
+-- title VARCHAR(50) NOT NULL,
+-- author VARCHAR(50),
+-- published_year INTEGER,
+-- price NUMERIC(10,2)
+-- )
+
+
+-- INSERT INTO book(title , author , published_year ,  price = 25.50)
+-- VALUES ('book1' , 'author1' , 2026 , 10.5),
+-- ('book2' , 'author2' , 2009 , 15),
+-- ('book3' , 'author3' , 2005 , 8)
+
+
+
+-- select * from book where published_year > 2010
+
+-- UPDATE book SET price = 25.50 WHERE book_id = 1
+
+-- select * from book ORDER BY price ASC
